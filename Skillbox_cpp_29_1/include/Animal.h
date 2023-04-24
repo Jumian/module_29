e6cda4adf967e3a14d5528bbd08692d0c0c05caa
Jumian <60074578+Jumian@users.noreply.github.com>
2023-04-24 10:10:16 +0200

    Fixed virtual class Talents use pure virtual

--- Skillbox_cpp_29_1/include/Animal.h
+++ Skillbox_cpp_29_1/include/Animal.h
@@ -3,7 +3,11 @@
 #include <iostream>
 #include <vector>
 
-class Talents;
+
+class Talents {
+public:
+	virtual std::string talentName()=0;
+};
 
 class Animal {
 protected:
@@ -11,36 +15,34 @@ protected:
 	std::vector<Talents*> talents;
 public:
 	virtual void voice()=0;
-	void showTalents() ;
-};
-
-class Talents {
-protected:
-	std::string name;
-public:
-	std::string talentName();
-};
-
-class Dance : virtual public Talents {
-public:
-	Dance() { name = "Dance"; };
+	void showTalents();
 	
 };
 
-class Swim : virtual public Talents {
+
+class Dance :  public Talents {
 public:
-	Swim() { name = "Swim"; }
-	std::string talentName() {
-		std::cout << "I can Swim";
-	};
+	std::string talentName() override { return "Dance"; };
 };
 
-class Dog : virtual public Animal {
+class Swim :  public Talents {
+public:
+	std::string talentName() override { return "Swim"; };
+};
+
+class Dog : public Animal {
 public:
 	explicit Dog(std::string _name) {
 		name = _name;
 		this->talents.push_back(new Dance());
 		this->talents.push_back(new Swim());
+	}
+	~Dog() {
+		for (int i = 0; i<talents.size();++i) {
+			if (talents.at(i) != nullptr) {
+				delete talents.at(i);
+			}
+		}
 	}
 	void voice() {
 		std::cout << "Bark! \n";
