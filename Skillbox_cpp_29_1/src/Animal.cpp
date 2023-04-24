e50a4a3fba32f4956813b0dd8a94310095548b2b
Jumian <60074578+Jumian@users.noreply.github.com>
2023-04-24 10:10:16 +0200

    Fixed virtual class Talents use pure virtual

--- Skillbox_cpp_29_1/src/Animal.cpp
+++ Skillbox_cpp_29_1/src/Animal.cpp
@@ -4,10 +4,6 @@ void Animal::showTalents(){
 	std::cout << "This is " << this->name << " and he has some talents:\n";
 for (int i = 0; i < this->talents.size();++i) {
 	std::cout << "\t";
-	this->talents.at(i)->talentName();
+	std::cout << "I can " << talents.at(i)->talentName() << std::endl;
 	}
-}
-std::string  Talents::talentName() {
-	std::cout << "I can " <<this->name << "\n";
-	return this->name;
 }
